c130882561f2a9e04bd66a31e3b17c9de07a997e
Keith Thomson <jkthomson@gmail.com>
2018-12-05 14:22:14 -0800

    first commit of project with basic solution in place

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,30 +31,55 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('have URL defined and URL not empty', function() {
+            for (feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            }
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have name defined and name not empty', function() {
+            for (feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('should be hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('should toggle visibility when menu icon clicked', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +87,54 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('should load at least one .entry into .feed container', function(done) {
+            const container = $('.feed');
+            expect(container).toBeDefined();
+            const entries = container.find('.entry');
+            expect(entries).toBeDefined();
+            expect(entries.length).toBeGreaterThan(0);
+            done();
+        });
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        var oldTextContent;
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            // load the first feed
+            loadFeed(0, function() {
+
+                // when that finishes, save the content of the first entry
+                var oldContainer = $('.feed');
+                var oldHeader= oldContainer.find('.entry').find('h2').get(0);
+                oldTextContent = oldHeader.textContent;
+
+                // now load the second feed
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+
+        it('should cause new content to be loaded', function(done) {
+            var newContainer = $('.feed');
+            var newHeader= newContainer.find('.entry').find('h2').get(0);
+            newTextContent = newHeader.textContent;
+            expect(newTextContent).not.toEqual(oldTextContent);
+            done();
+        });
+
+    });
 }());
